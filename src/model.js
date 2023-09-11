9d83093918769f9450d78c567ddf3fc78429bb20
KiraYagamov <isaru@sasuke.ru>
2023-09-11 18:36:54 +0300

    First commit

---
+++ src/model.js
@@ -0,0 +1,12 @@
+import image from './assets/image.png'
+export const model = [
+    {type: "title", value:"Hello world JS!"},
+    {type: "text", value: "This is a text"},
+    {type: "columns", value: [
+            '1111',
+            '2222',
+            '3333',
+            '4444'
+        ]
+    }
+]
