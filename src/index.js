d6f816692bdc262a32b2ce278762caacb3396f97
KiraYagamov <105204832+KiraYagamov@users.noreply.github.com>
2023-09-12 19:27:13 +0300

    a

--- src/index.js
+++ src/index.js
@@ -1,22 +1,34 @@
-import {templates} from './templates'
-import {model} from './model'
-import './styles/main.css'
+import React, {createContext} from 'react'
+import ReactDOM from 'react-dom'
+import App from './App'
 import { initializeApp } from 'firebase/app';
-import { getDatabase, ref, set, onValue, get  } from "firebase/database";
-import { getAuth, signInWithEmailAndPassword } from "firebase/auth";
+import { getAuth } from "firebase/auth";
+import {getDatabase} from "firebase/database";
+
 
 const firebaseConfig = {
     apiKey: "AIzaSyC_0Mwc1D_iMA6fpvtG8bjcTdGcfnfdpIg",
     authDomain: "bomes-fca0a.firebaseapp.com",
-    databaseURL: "https://bomes-fca0a-default-rtdb.asia-southeast1.firebasedatabase.app/",
+    databaseURL: "https://bomes-fca0a-default-rtdb.asia-southeast1.firebasedatabase.app",
     projectId: "bomes-fca0a",
     storageBucket: "bomes-fca0a.appspot.com",
     messagingSenderId: "1047127596102",
     appId: "1:1047127596102:web:44e24f7e847885ccc8fe47",
     measurementId: "G-8NFGCVM37Y"
 };
-
-const $site = document.querySelector("#site")
 const app = initializeApp(firebaseConfig);
-const auth = getAuth(app);
-const database = getDatabase(app);
+
+export const Context = createContext(null)
+
+const auth = getAuth()
+const database = getDatabase()
+
+ReactDOM.render(
+    <Context.Provider value={{
+        auth,
+        database
+    }}>
+        <App/>
+    </Context.Provider>,
+    document.getElementById('root')
+)
